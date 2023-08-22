dfea798ff73630c1f175ccb42fb0527c719972f5
zahidhasan13 <zahidhasanofficial13@gmail.com>
2023-08-22 23:08:09 +0600

    delete data

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable react/prop-types */
 
-const User = ({user}) => {
+const User = ({user, handleDelete, handleUpdate}) => {
+    
     return (
         <div className="user">
             <h2>{user.name}</h2>
             <p>{user.email}</p>
+            <div className="btn">
+                <button onClick={() => handleDelete(user._id)}>Delete</button>
+                <button onClick={handleUpdate}>Update</button>
+            </div>
         </div>
     );
 };
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,8 +1,32 @@
 import { useLoaderData } from "react-router-dom";
 import User from "./User";
+import { useState } from "react";
 
 const Users = () => {
-  const users = useLoaderData();
+  const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    fetch(`http://localhost:5000/users/${_id}`,{
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            _id: _id,
+        }),
+    })
+    .then(res => res.json())
+    .then(data => {
+        if(data.deletedCount>0){
+            setUsers(users.filter(user => user._id!== _id));
+        }
+    })
+};
+const handleUpdate = () => {};
+
+  console.log(users);
   return (
     <div>
       <h1>Users: {users.length}</h1>
@@ -11,6 +35,8 @@ const Users = () => {
             users.map(user => <User
             key={user._id}
             user={user}
+            handleUpdate={handleUpdate}
+            handleDelete={handleDelete}
             ></User>)
         }
       </div>
